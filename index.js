b92877a8ce1ea08e6ed267a92b03f27af7979967
root <mridhohafidz12@gmail.com>
2022-10-11 06:12:42 +0000

    update config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello, my name is ridhohafidz" }');
+    res.send('{ "response": "Hello From ridhohafidz" }');
 });
 
 app.get('/will', function (req, res) {
